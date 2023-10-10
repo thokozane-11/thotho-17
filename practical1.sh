1e1e07208d5afef164f6570d483dc80826af7200
thokozane-11 <147553986+thokozane-11@users.noreply.github.com>
2023-10-10 23:10:48 +0200

    Create practical1.sh

---
+++ practical1.sh
@@ -0,0 +1,36 @@
+sudo apt-get update
+sudo apt-get -y install nginx
+systemctl status nginx
+systemctl is-active nginx
+systemct1 start nginx
+sudo systemctl start nginx
+systemctl is-active nginx
+ls
+cd ~
+ls
+sudo mkdir /var/www/html/labs
+ls -ld /var/www/html/labs
+sudo chown thoko:root /var/www/html/labs
+ls -ld /var/www/html/labs
+ln -s /var/www/labs html
+ls -l
+cd html
+touch index.html
+nano 
+nano index.html
+cat index.html
+mkdir bin
+cd bin
+touch websrv_config_script
+ls -1
+chmod u+x websrv_config_scrpt.bash
+ls -l
+./websrv_config_script.bash
+nano websrv_config_script.bash
+.websrv_config_script.bash
+nano websrv_config_script.bash
+./websrv_config_script.bash
+sudo systemctl stop nginx
+./websrv_config_script.bash
+
+
